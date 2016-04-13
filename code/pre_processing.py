1d4a7653332dd600f9e384fc1a9d91dee90d4bda
Francesco Ciuci <fcesco>
2016-04-13 15:10:38 +0200

    init preprocessing

---
+++ code/pre_processing.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import json
+
+attribute = pd.read_csv('../input/attributes.csv')
+product_description = pd.read_csv('../input/product_descriptions.csv')
+test = pd.read_csv('../input/test.csv')
+train = pd.read_csv('../input/train.csv')
